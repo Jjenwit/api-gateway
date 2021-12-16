0a7737b6c6814ad3cd9c369685ae0f256434c17b
James <jameskrap123@gmail.com>
2021-12-17 04:18:27 +0700

    add cors

--- src/Server.ts
+++ src/Server.ts
@@ -1,46 +1,48 @@
-import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
-import helmet from 'helmet';
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import helmet from 'helmet'
 
-import express, { NextFunction, Request, Response } from 'express';
-import StatusCodes from 'http-status-codes';
-import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express'
+import StatusCodes from 'http-status-codes'
+import 'express-async-errors'
 
-import BaseRouter from './routes';
-import logger from '@shared/Logger';
+import BaseRouter from './routes'
+import logger from '@shared/Logger'
+import cors from 'cors'
 
-const app = express();
-const { BAD_REQUEST } = StatusCodes;
+const app = express()
+const { BAD_REQUEST } = StatusCodes
 
 /************************************************************************************
  *                              Set basic express settings
  ***********************************************************************************/
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cors({ origin: '*', optionsSuccessStatus: 200 }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 // Show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+     app.use(morgan('dev'))
 }
 
 // Security
 if (process.env.NODE_ENV === 'production') {
-  app.use(helmet());
+     app.use(helmet())
 }
 
 // Add APIs
-app.use('/api', BaseRouter);
+app.use('/api', BaseRouter)
 
 // Print API errors
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  logger.err(err, true);
-  return res.status(BAD_REQUEST).json({
-    error: err.message,
-  });
-});
+     logger.err(err, true)
+     return res.status(BAD_REQUEST).json({
+          error: err.message,
+     })
+})
 
 // Export express instance
-export default app;
+export default app
